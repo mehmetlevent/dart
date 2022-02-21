c0c95cbc3e6bbea30dcf84ca1871325a9a4436fc
mehmetlevent <41065086+mehmetlevent@users.noreply.github.com>
2022-02-21 10:24:33 +0300

    Add files via upload

---
+++ string_extension.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  if ('ali'.isAdmin()) {
+    print('object');
+  }
+  String? name;
+
+  name.isAdmin();
+}
+
+extension StringUserCheckExtension on String? {
+  bool isAdmin() {
+    return (this ?? '').toLowerCase() == 'admin'.toLowerCase();
+  }
+}
